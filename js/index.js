f2c8bb8944e82880d991877b9e524e923db10f9f
Fernand Tan <ferdyboy81@gmail.com>
2016-12-05 23:48:33 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+(function (){
+  var body = document.getElementsByTagName('body')[0];
+  var navbar = document.getElementById('legacy-nav');
+  var videoContainer = document.getElementById('splash-video');
+  var video = document.getElementById('video');
+
+  function changeNavbarStyle() {
+    if (body.scrollTop < 5) {
+      navbar.className = 'row'
+    } else {
+      navbar.className = 'row show'
+    }
+  };
+
+  // video resize
+  function stretchVideo() {
+    if(window.innerWidth/window.innerHeight < 1.3){
+      video.style.height = '100%';
+      video.style.width = 'auto';
+    } else {
+      video.style.width = '100%';
+      video.style.height = 'auto';
+    }
+  }
+
+  stretchVideo();
+  changeNavbarStyle();
+
+  window.addEventListener("scroll", changeNavbarStyle);
+  window.addEventListener("resize", stretchVideo);
+
+}
+)();
