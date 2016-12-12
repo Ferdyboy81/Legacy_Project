70e0d64f9df4a9a23d7ecc3e1737276ee7b03be8
Daniel Mendoza <mendozad05@yahoo.com>
2016-12-11 18:33:37 -0800

    added nabber mobile styles

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
 (function (){
   var body = document.getElementsByTagName('body')[0];
-  var navbar = document.getElementById('legacy-nav');
+  var navbar = document.getElementById('legacy-navbar');
+  var navbarLinks = document.getElementsByClassName('legacy-navbar-links')[0];
+  var mobileMenuButton = document.getElementsByClassName('legacy-mobile-menu')[0];
   var videoContainer = document.getElementById('splash-video');
   var video = document.getElementById('video');
 
+  var mobileLinkActive = false;
+  var currentScrollTop = 0;
+
   function changeNavbarStyle() {
+    currentScrollTop = body.scrollTop;
     if (body.scrollTop < 100) { // if scroll less than 100px nav fades away
-      navbar.className = 'row' // navbar fades
+      navbar.className = '' // navbar fades
     } else {
-      navbar.className = 'row show' // creates new class
+      navbar.className = 'show' // creates new class
+    }
+
+    if (body.scrollTop > 600 && currentScrollTop > lastScrollTop) {
+      navbar.className = 'show slide-up'
+    }
+    lastScrollTop = currentScrollTop;
+  };
+
+  function openMobileNavigation() {
+    if(!mobileLinkActive) {
+      body.className = 'stopScrolling';
+      navbarLinks.className = "legacy-navbar-links active"
+      mobileLinkActive = true;
+    } else {
+      body.className = '';
+      navbarLinks.className = "legacy-navbar-links"
+      mobileLinkActive = false;
     }
   };
 
@@ -26,6 +49,7 @@
   stretchVideo();
   changeNavbarStyle();
 
+  mobileMenuButton.addEventListener("click", openMobileNavigation);
   window.addEventListener("scroll", changeNavbarStyle);
   window.addEventListener("resize", stretchVideo);
 
